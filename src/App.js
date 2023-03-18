969ddd734964ebfb8ca37c2fc2b44572be7f9410
Amal Vitorino <69655550+amalvitorino@users.noreply.github.com>
2023-03-18 23:50:31 +0200

    first commit
    puxando o app para o repositorio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import Container from './components/layout/Container';
+import Footer from './components/layout/Footer';
+import Navabar from './components/layout/Navbar';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import Home from './components/pages/Home';
+import NewProject from './components/pages/NewProject';
+import Project from './components/pages/Projects';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      
+      <Navabar/>
+
+      <Container costumClass="min-height">
+        
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/project' element={<Project />} />
+          <Route path='/company' element={<Company />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/newproject' element={<NewProject />} />
+
+        </Routes>
+      </Container>
+
+      <Footer />
+    </Router>
+
+  )
 }
 
 export default App;
